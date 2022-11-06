3ec89fb73206f81a59334fc505cbbfeebac7a4eb
adi247u <104594678+adityavag@users.noreply.github.com>
2022-11-06 22:46:49 +0530

    #9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Fade} from 'react-reveal'
 import { BsCodeSlash } from 'react-icons/bs'
 export default function Footer() {
   return (
-    <div className='flex flex-col items-center  bg-[#191b24] w-full py-2'>
+    <Fade right>
+      <div className='flex flex-col items-center  bg-[#191b24] w-full py-2'>
         <a href="https://github.com/adityavag/portfolio"><BsCodeSlash className='animate-pulse text-[#ccd6f6]'/></a>
-    </div>
+      </div>
+    </Fade>
   )
 }
