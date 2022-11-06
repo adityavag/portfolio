6782f27d233aaf802d08c05fa3cc4acd700d086d
adi247u <104594678+adityavag@users.noreply.github.com>
2022-11-06 22:43:04 +0530

    #9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsCodeSlash } from 'react-icons/bs'
 export default function Footer() {
   return (
     <div className='flex flex-col items-center  bg-[#191b24] w-full py-2'>
-        <a href="https://github.com/adityavag/portfolio"><BsCodeSlash className='text-[#ccd6f6]'/></a>
+        <a href="https://github.com/adityavag/portfolio"><BsCodeSlash className='animate-pulse text-[#ccd6f6]'/></a>
     </div>
   )
 }
